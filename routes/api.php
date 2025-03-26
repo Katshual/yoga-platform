53d5bf0dcb873593b6cde5433e4930d500d80992
Katshual <139873320+Katshual@users.noreply.github.com>
2025-03-26 15:22:28 +0100

    mise à jour du fichier api.php avec les routes et les commentaires

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//import du contrôleur PostureController
+use App\Http\Controllers\PostureController;
+
+/* Définition des routes RESTful pour la ressource "postures".
+ Cette ligne génère automatiquement les routes suivantes :
+ GET /postures          -> Liste toutes les postures
+ POST /postures         -> Crée une nouvelle posture
+ GET /postures/{id}     -> Affiche une posture spécifique
+ PUT/PATCH /postures/{id} -> Met à jour une posture spécifique
+ DELETE /postures/{id}  -> Supprime une posture spécifique
+ */
+
+Route::apiResource('postures', PostureController::class);
+
+/* Route protégée par Sanctum qui renvoie les informations de l'utilisateur authentifié.
+ Nécessite un token valide dans l'en-tête Authorization pour y accéder.
+Utilisée pour vérifier l'authentification et récupérer les données de l'utilisateur.
+*/
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
